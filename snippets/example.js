b61f3df636d630506e253f9621528bf6c1ca9406
Sebastian Wilzbach <sebi@wilzbach.me>
2014-10-06 04:12:35 +0200

    snippet comment instance var

--- snippets/example.js
+++ snippets/example.js
@@ -7,6 +7,7 @@ var data = [{name: "A", size: 4, imports: ["B"]},
 yourDiv.textContent = "";
 var config = {el: yourDiv, diameter:460, diffInnerRadius: 20,tension: 0.85, data: data};
 var obj = new circular(config);
+//instance=obj
 obj.on("mouseover", function(d){
   console.log(d);
 });
--- snippets/example2.js
+++ snippets/example2.js
@@ -3,6 +3,7 @@ yourDiv.textContent = "";
 d3.json("http://www.corsproxy.com/bl.ocks.org/mbostock/raw/7607999/a782ae3d8a76201e9f46983166012121d6138361/readme-flare-imports.json", function(error, data) {
   var config = {el: yourDiv, diameter:960, tension: 0.85, data: data};
   var obj = new circular(config);
+  //instance=obj
   obj.on("mouseover", function(d){
     console.log(d);
   });
